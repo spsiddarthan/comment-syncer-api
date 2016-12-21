97ae6ac3304d254e0f589cc058c5bc339c1fb15d
Sidocker <siddarthan@outlook.com>
2016-12-22 00:12:59 +0530

    Allow cross origin requests

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var postComments = require('./comments/post.js');
 var postMessages = require('./queues/post.js');
